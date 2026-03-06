9ce528460149c02a47335c8d13083fa2a7e38e66
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2026-03-06 17:44:58 -0300

    ControllersDataBase 0.0.0.6
    Criação dos crontrollers para trabalho da api com get,put etc

---
+++ DVHB/backend/src/controllers/productController.js
@@ -0,0 +1,66 @@
+const Product = require('../models/product.model');
+
+exports.getProducts = async (req, res, next) => {
+    try {
+        const products = await Product.findAll();
+        res.json(products);
+    }
+    catch (err) {
+        next(err);
+    }
+};
+
+exports.getProductById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByPk(id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json(product);
+            }catch (err) {
+        next(err);
+        }
+};
+
+exports.createProduct = async (req, res, next) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+        }catch (err) {
+        next(err);
+    }
+};
+
+exports.updateProduct = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+
+        const product = await Product.findByPk(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        await product.update(req.body);
+        res.json(product);
+    }
+        catch (err) {
+        next(err);
+    }
+};
+
+exports.deleteProduct = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+
+        const product = await Product.findByPk(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+    }
+    await product.destroy();
+    res.status(204).send();
+    }
+    catch (err) {
+        next(err);
+        }
+};
