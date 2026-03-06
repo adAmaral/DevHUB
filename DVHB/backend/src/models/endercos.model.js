d638ee6b8b7293f7fd7685f2b06e75ddb65e6b12
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2026-03-06 16:19:39 -0300

    Json's DataBase 0.0.0.3

--- DVHB/backend/src/models/endercos.model.js
+++ DVHB/backend/src/models/endercos.model.js
@@ -1,10 +1,10 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/db');
-const Endereco = sequelize.define('Endereco', {
+const Enderco = sequelize.define('Enderco', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     usuario_id: { type: DataTypes.INTEGER, allowNull: false },
     tipo: { type: DataTypes.ENUM('residencial', 'comercial'), allowNull: false },
-    Endereco: { type: DataTypes.STRING, allowNull: false },
+    endereco: { type: DataTypes.STRING, allowNull: false },
     numero: { type: DataTypes.STRING, allowNull: false },
     complemento: { type: DataTypes.STRING, allowNull: true },
     bairro: { type: DataTypes.STRING, allowNull: false },
@@ -15,7 +15,7 @@ const Endereco = sequelize.define('Endereco', {
     data_criacao: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 },
     {
-        tableName: 'enderecos',
+        tableName: 'endercos',
         timestamps: false,
     });
 module.exports = Endereco;
